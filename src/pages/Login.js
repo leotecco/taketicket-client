e9fabf0a66791c492a7426afef3ac0207338a531
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2020-10-20 20:11:38 -0300

    Ajusta campos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,24 +6,23 @@ import { Form, Button } from './../components'
 import LogoTackTicket from './../images/logo-take-ticket.svg'
 
 const Login = () => {
-    return <div className="flex flex-col justify-between py-20 px-10 h-full">
+  return <div className="flex flex-col justify-between py-20 px-10 h-full">
+    <div className="flex flex-col">
+      <img src={LogoTackTicket} className="w-60 mb-16" alt="Logo TakeTicket" />
 
-        <div className="flex flex-col">
-            <img src={LogoTackTicket} className="w-60 mb-16" alt="Logo TakeTicket" />
+      <Form.Input label="E-mail" className="mb-2" />
 
-            <Form.Input label="E-mail" className="mb-2" />
+      <Form.Input label="Senha" className="mb-12" type="password" />
 
-            <Form.Input label="Senha" className="mb-12" type="password" />
+      <Button color="blue-600" textColor="white" className="mb-2">Entrar</Button>
 
-            <Button color="blue-600" textColor="white" className="mb-2">Entrar</Button>
-
-            <Link to="/register">
-                <Button>Inscrever-se</Button>
-            </Link>
-        </div>
-
-        <p className="text-sm text-gray-500">Todos os diretos reservados © TakeTicket 2020</p>
+      <Link to="/register">
+        <Button>Inscrever-se</Button>
+      </Link>
     </div>
+
+    <p className="text-sm text-gray-500">Todos os diretos reservados © TakeTicket 2020</p>
+  </div>
 }
 
 export default Login
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,26 +7,32 @@ import { Form, Button } from './../components'
 import LogoTackTicket from './../images/logo-take-ticket.svg'
 
 const Register = () => {
-    return <div className="flex flex-col justify-between py-20 px-10 h-full">
+  return <div className="flex flex-col justify-between py-20 px-10 h-full">
 
-        <div className="flex flex-col">
-            <img src={LogoTackTicket} className="w-60 mb-16" alt="Logo TakeTicket" />
+    <div className="flex flex-col">
+      <img src={LogoTackTicket} className="w-60 mb-16" alt="Logo TakeTicket" />
 
-            <Form.Input label="E-mail" className="mb-2" />
+      <Form.Input label="Nome" id="name" className="mb-2" />
 
-            <Form.Input label="CPF ou CNPJ" className="mb-2" />
+      <Form.Input label="Razão Social" id="companyName" className="mb-2" />
 
-            <Form.Input label="Senha" className="mb-12" type="password" />
+      <Form.Input label="E-mail" id="email" className="mb-2" />
 
-            <Button color="blue-600" textColor="white" className="mb-2">Inscrever-se</Button>
+      <Form.Input label="CPF" id="cpf" className="mb-2" />
 
-            <Link to="/login">
-                <Button>Voltar</Button>
-            </Link>
-        </div>
+      <Form.Input label="CNPJ" id="cnpj" className="mb-2" />
 
-        <p className="text-sm text-gray-500">Todos os diretos reservados © TakeTicket 2020</p>
+      <Form.Input label="Senha" id="password" className="mb-12" type="password" />
+
+      <Button color="blue-600" textColor="white" className="mb-2">Inscrever-se</Button>
+
+      <Link to="/login" className="mb-12">
+        <Button>Voltar</Button>
+      </Link>
     </div>
+
+    <p className="text-sm text-gray-500 mb-4">Todos os diretos reservados © TakeTicket 2020</p>
+  </div>
 }
 
 export default Register
