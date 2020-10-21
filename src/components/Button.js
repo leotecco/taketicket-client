3a5c783cdffccf7070fe30ad343c6cc4ae427c48
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2020-10-20 21:36:31 -0300

    Ajusta formatação

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = ({ className, children, color = "white", textColor = "gray-700", ...rest }) => {
+const Button = ({ className, children, color = 'white', textColor = 'gray-700', ...rest }) => {
 
-    return <button type="button" className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-${color} leading-6 font-medium text-${textColor} shadow-sm focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 ${className}`} {...rest}>
-        {children}
-    </button>
+  return <button type="button" className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-${color} leading-6 font-medium text-${textColor} shadow-sm focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 ${className}`} {...rest}>
+    {children}
+  </button>
 }
 
 export default Button
